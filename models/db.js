c95c1201dbaede23fef4390b590bee162a8086c5
Bharathdev07 <devops854@gmail.com>
2024-06-15 14:29:52 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect('mongodb://localhost:27017/db_test', { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false  });
+const url = `${process.env.MONGOURL}/db_test` || "mongodb://localhost:27017/db_test";
+mongoose.connect(url, { useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify:false  });
 // Validation
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
